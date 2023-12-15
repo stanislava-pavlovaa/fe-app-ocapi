03960132f35e8e101ffed4ee444483a6ef0c8722
stanislava-pavlovaa <stanislava.pavlova@c4nexus.com>
2023-12-15 17:50:19 +0200

    Add billing form and add billing to basket

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import ShippingForm from './ShippingForm';
+import BillingForm from './BillingForm';
 
 const Checkout = () => {
   const [isShippingFormReady, setIsShippingFormReady] = useState(false);
+  const [isBillingFormReady, setIsBillingFormReady] = useState(false);
 
-  if (!isShippingFormReady) {
+  if (!isShippingFormReady && !isBillingFormReady) {
+    return <ShippingForm setIsShippingFormReady={setIsShippingFormReady} />;
+  } else if (isShippingFormReady && !isBillingFormReady) {
+    return <BillingForm setIsBillingFormReady={setIsBillingFormReady} />;
+  } else if (isShippingFormReady && isBillingFormReady) {
     return (
-      <ShippingForm setIsShippingFormReady={setIsShippingFormReady}></ShippingForm>
+      <p>Ready</p>
     );
-  } else {
-    return <p>dada</p>
   }
 };
 
